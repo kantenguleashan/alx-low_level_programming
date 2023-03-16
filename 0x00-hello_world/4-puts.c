3cfa21cc340981e3d052ea235b89671020e0b848
kantenguleashan <kantenguleshan@gmail.com>
2023-03-16 16:19:10 -0700

    prgram

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*The progrma print a line 
+*and the function is puts
+*The return is 0
+*/
+
+int main(void)
+{  puts("\"Programming is like building a multilingual puzzle");
+   return (0);
+
+}
