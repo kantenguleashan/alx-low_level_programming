b6f42f9d01fece752ebfd58c33579039a6cd6024
kantenguleashan <kantenguleshan@gmail.com>
2023-03-16 16:43:54 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-/**
-*The progrma print a line 
-*and the function is puts
-*The return is 0
-*/
-
+ /**
+  *The progrma print a line 
+and the function is puts
+ *The return is 0
+ */
 int main(void)
-{  puts("\"Programming is like building a multilingual puzzle");
-   return (0);
-
+{puts("\"Programming is like building a multilingual 
+puzzle");
+return (0);
 }
