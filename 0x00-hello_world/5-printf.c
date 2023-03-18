c6a121fe18f0d3b3bfaf142ac81148b89af67231
kantenguleashan <kantenguleshan@gmail.com>
2023-03-18 06:06:45 -0700

    5 compiler

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/*Th main function*/
 
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
-/*The function will start to execute*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return	(0);
+	return (0);
 }
