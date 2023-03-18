a7e64508ad1cc7504bbc3364b66d6ce4b32729a7
kantenguleashan <kantenguleshan@gmail.com>
2023-03-18 06:04:40 -0700

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-/*The main	function prints the line*/
-int	main(void)
-{	puts("\"Programming is like building a multilingual puzzle");/*The line */
-	return	(0);/*The return is 0*/
+
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
