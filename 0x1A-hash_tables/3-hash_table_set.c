39c7c5a03cff7ec61bf240639f0c263f0bfc9236
kantenguleashan <kantenguleshan@gmail.com>
2023-09-08 14:47:51 -0700

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,9 +2,11 @@
 
 /**
  * hash_table_set - Add or update an element in a hash table.
- * @ht: A pointer to the hash table
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
  *
- * Return: 0 or 1 when successful
+ * Return: Upon failure - 0.
  *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
