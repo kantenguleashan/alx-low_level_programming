c26cede8ed8bf7536a2a7936aa9316cb8035c98b
kantenguleashan <kantenguleshan@gmail.com>
2023-09-08 18:39:14 -0700

    6 hash

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,11 +10,11 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
-	for (z = 0; z < ht->size; z++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[z] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			node = ht->array[z];
+			node = ht->array[i];
 			while (node != NULL)
 			{
 				tmp = node->next;
@@ -28,4 +28,3 @@ void hash_table_delete(hash_table_t *ht)
 	free(head->array);
 	free(head);
 }
-
